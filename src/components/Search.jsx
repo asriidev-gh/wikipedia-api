55e3c65b89651744e6cd3439731b264eaa3b3895
Alejandro Radam II <alejandro.radam@eastvantage-europe.com>
2023-06-28 17:15:06 +0800

    Wikepedia Search API

---
+++ src/components/Search.jsx
@@ -0,0 +1,50 @@
+import logo from '../logo.png';
+import Word from './Words';
+import { useState } from 'react';
+
+function Search() {
+  const [result, setResult] = useState({});
+
+  const fetchData = async (val) => {
+    const trimVal = val.trim().toLowerCase();
+    if (trimVal.length > 0) {
+      const api = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${trimVal}`;
+      const response = await fetch(api);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return await response.json();
+    }
+  };
+
+  const handleSearch = async (e) => {
+    const { value } = e.target;
+    setResult({ result: await fetchData(value) });
+  };
+
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="caption">
+          <h4>WIKIPEDIA</h4>
+          <p>The Free Encyclopedia</p>
+        </div>
+        <img src={logo} alt="" />
+      </div>
+      <div className="content">
+        <form action="">
+          <input type="text" className="search" onKeyUp={handleSearch} />
+        </form>
+      </div>
+      <div className="word">
+        <ul>
+          {result.result &&
+            result.result.query.search.map((data) => (
+              <Word key={data.id} data={data} />
+            ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+export default Search;
---
+++ src/components/Words.jsx
@@ -0,0 +1,13 @@
+function Word(props) {
+  const searchList = document.querySelector('.listData');
+  searchList &&
+    searchList.insertAdjacentHTML(
+      'afterbegin',
+      `<li>
+		  <h4>${props.data.title}</h4>
+		  ${props.data.snippet}
+	  </li>`
+    );
+  return <div className="listData"></div>;
+}
+export default Word;
